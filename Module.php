b2c4d1e4ced94202e345dfaa3ca870dde2ab69f3
sash04ek <a.ovcharov@gmail.com>
2018-04-20 09:49:30 +0300

    Quota fixes

--- Module.php
+++ Module.php
@@ -1,5 +1,8 @@
 <?php
 /**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AGPL-3.0 or AfterLogic Software License
+ *
  * This code is licensed under AGPLv3 license or AfterLogic Software License
  * if commercial version of the product was purchased.
  * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
@@ -8,10 +11,6 @@
 namespace Aurora\Modules\CorporateFiles;
 
 /**
- * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
- * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
- * @copyright Copyright (c) 2018, Afterlogic Corp.
- *
  * @package Modules
  */
 class Module extends \Aurora\Modules\PersonalFiles\Module
@@ -21,6 +20,9 @@ class Module extends \Aurora\Modules\PersonalFiles\Module
 	public function init() 
 	{
 		parent::init();
+		
+		$this->subscribeEvent('Files::GetQuota::after', array($this, 'onAfterGetQuota'));
+		
 		$this->RemoveEntries(
 			array(
 				'upload',
@@ -29,5 +31,30 @@ class Module extends \Aurora\Modules\PersonalFiles\Module
 		);
 	}
 	
-	
+	public function onAfterGetQuota($aArgs, &$mResult)
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+		
+		if ($this->checkStorageType($aArgs['Type']))
+		{
+			$sPublicUserId = \Aurora\System\Api::getUserPublicIdById($aArgs['UserId']);
+			
+			$oDirectory = $this->oApiFilesManager->getDirectory($sPublicUserId, $aArgs['Type'], '/');
+			$iSize = 0;
+			if ($oDirectory)
+			{
+				$sPath = $oDirectory->getPath();
+				
+				$aSizeInfo = \Aurora\System\Utils::GetDirectorySize($sPath);
+				$iSize = $aSizeInfo['size'];
+			}
+			
+			$mResult = array(
+				'Used' => $iSize,
+				'Limit' => $this->getConfig('UserSpaceLimitMb', 0) * 1024 * 1024
+			);
+			
+			return true;
+		}
+	}
 }
